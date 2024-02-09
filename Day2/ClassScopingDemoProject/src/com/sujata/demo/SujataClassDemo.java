3f3aa2da22e4f915904a9f9dc664fc9ce390ec69
Sujatabatra <sujatabatra@hotmail.com>
2024-02-09 10:51:57 +0530

    Add files via upload

---
+++ Day2/ClassScopingDemoProject/src/com/sujata/demo/SujataClassDemo.java
@@ -0,0 +1,29 @@
+package com.sujata.demo;
+
+class FirstClass {
+
+	public void show() {
+		System.out.println("First Class");
+	}
+}
+
+class SecondClass {
+
+	public void show() {
+		System.out.println("Second Class");
+	}
+}
+
+class ThirdClass {
+
+	public void show() {
+		System.out.println("Third Class");
+	}
+}
+
+class FourthClass {
+
+	public void show() {
+		System.out.println("Fourth Class");
+	}
+}
---
+++ Day2/ClassScopingDemoProject/src/com/sujata/training/FirstClass.java
@@ -0,0 +1,29 @@
+package com.sujata.training;
+
+public class FirstClass {
+
+	public void show() {
+		System.out.println("First Class");
+	}
+}
+
+class SecondClass {
+
+	public void show() {
+		System.out.println("Second Class");
+	}
+}
+
+class ThirdClass {
+
+	public void show() {
+		System.out.println("Third Class");
+	}
+}
+
+class FourthClass {
+
+	public void show() {
+		System.out.println("Fourth Class");
+	}
+}
